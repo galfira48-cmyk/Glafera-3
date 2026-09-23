a733f2477dd5d34cf2a667248d8d36c8ae5028c4
galfira48-cmyk <galfira48@gmail.com>
2026-09-23 20:59:38 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'glavera3-v9';
+const CACHE_NAME = 'glavera3-v10';
 const CORE_ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './music.mp3'];
 
 self.addEventListener('install', (event) => {
